0536a0ec68ce3dc6f7a3ba4dd245bcb0c9fdac01
Samuel <colaco.samuel8@gmail.com>
2024-09-27 17:00:19 -0300

    Baixando o knexJS e sqlite, e criando o kenxfile

---
+++ knexfile.ts
@@ -0,0 +1,45 @@
+
+
+
+export default {
+    client: "sqlite3",
+    connection: {
+        filename: "./src/database/database.db",
+    },
+    userNullAsDefault: true,
+    migrations: {
+        extensions: "ts",
+        directory: "./src/database/migrations"
+    },
+    seeds: {
+        extensions: "ts",
+        directory: "./src/database/seeds"
+    },
+}
+
+/*
+import { Knex } from "knex"
+
+const config : {[key: string]: Knex.Config} = {
+    development : {
+        client: "sqlite3",
+        connection: {
+            filename: "./src/database/database.db",
+        },
+
+        useNullAsDefault: true,
+        migrations: {
+            extension: "ts",
+            directory: "./src/database/migrations",
+        },
+    
+        seeds: {
+            extension: "ts",
+            directory: "./src/database/seeds" ,
+        },
+    },
+
+}
+
+export default config
+*/
