bb3930771f96974faaa34219b79031da9ecc17c8
Samuel <colaco.samuel8@gmail.com>
2024-10-11 15:46:17 -0300

    Criando a rota POST de tables-sessions

--- src/controller/tables-sessions-controller.ts
+++ src/controller/tables-sessions-controller.ts
@@ -1,9 +1,21 @@
 
 import { Request, Response, NextFunction} from "express"
+import { knex } from "../database/knex"
+import z from "zod"
 
 export class TablesSessions{
     async create(req: Request, res: Response, next: NextFunction){
         try {
+            const { tables_id } = req.body
+
+            const bodySchema = z.object({
+                tables_id: z.number().gt(0)
+            })
+            bodySchema.parse(req.body)
+            
+            await knex.raw('PRAGMA foreign_keys = ON');
+            await knex<TablesSessionsTypes>("tables-sessions").insert({tables_id,opened_at: knex.fn.now() }) 
+            
             res.status(201).json()
         } catch (error) {
             next(error)
---
+++ src/types/tables-sessions.d.ts
@@ -0,0 +1,7 @@
+
+type TablesSessionsTypes = {
+    id: number,
+    tables_id: number,
+    opened_at: number,
+    closed_at_time: number
+}
