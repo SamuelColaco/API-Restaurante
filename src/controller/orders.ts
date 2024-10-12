51d58841aea32f2d04a1ad1a3c02122a8da7f1db
Samuel <colaco.samuel8@gmail.com>
2024-10-12 15:47:10 -0300

    Criando a rota de total de orders

--- src/controller/orders.ts
+++ src/controller/orders.ts
@@ -2,13 +2,19 @@
 import {Request, Response, NextFunction } from "express"
 import { knex } from "../database/knex"
 import z from "zod"
+import { AppError } from "../utils/AppError"
+import { totalmem } from "os"
 export class Orders{
     async index(req: Request, res: Response, next: NextFunction){
         try {
+            const { table_sessions_id } = req.params
+
+            const orders =   await knex("Pedidos")
+            .select("Pedidos.id", "Pedidos.table_sessions_id", "Pedidos.products_id", "products.name","Pedidos.quantity", "Pedidos.price AS order_price")
+            .join("products", "products.id", "Pedidos.products_id")
+            .where({ table_sessions_id })
             
-            const orders =   await knex<OrdersRepository>("Pedidos").select()
-            
-            res.status(200).json(orders)
+            res.status(200).json(orders.length === 0 ? 'Sem Pedidos': orders )
         } catch (error) {
             next(error)
         }
@@ -28,10 +34,37 @@ export class Orders{
             await knex.raw('PRAGMA foreign_keys = ON');
             const priceProducts = (await knex<ProductRepository>("products").where({ id: products_id }).pluck('price'))
 
+            const tableSessions = await knex<TablesSessionsTypes>("tables-sessions").where({ id: table_sessions_id}).first()
+
             const price = quantity * Number(priceProducts)
-            console.log(price)
+
+            if(!tableSessions){
+                throw new AppError("Sessão não existe")
+            }
+
+            if(tableSessions.closed_at_time){
+                throw new AppError("Sessão já foi fechada")
+            }
+
             await knex<OrdersRepository>("Pedidos").insert({ quantity, table_sessions_id, products_id, price})
             res.status(201).json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async show(req: Request, res: Response, next: NextFunction){
+        try {
+
+            const { table_sessions_id } = req.params
+            
+            const array = await knex("Pedidos").where({ table_sessions_id }).pluck('price')
+            let total = 0
+            for(let i = 0; i < array.length; i++){
+                total += Number(array[i])
+            }
+            res.status(200).json({ "total": total})
+            
         } catch (error) {
             next(error)
         }
--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -6,7 +6,8 @@ import { Orders } from "../controller/orders"
 const ordersRoutes = Router()
 const ordersController = new Orders()
 
-ordersRoutes.get("/orders", ordersController.index)
+ordersRoutes.get("/orders/table/:table_sessions_id", ordersController.index)
 ordersRoutes.post("/orders", ordersController.create)
+ordersRoutes.get("/orders/table/total/:table_sessions_id", ordersController.show)
 
 export { ordersRoutes } 
