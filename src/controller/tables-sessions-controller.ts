897373d034e4e769471aa6bbb6505f0034d41cdf
Samuel <colaco.samuel8@gmail.com>
2024-10-12 13:37:12 -0300

    Criando a rota GET e PUT

--- src/controller/tables-sessions-controller.ts
+++ src/controller/tables-sessions-controller.ts
@@ -5,6 +5,16 @@ import z from "zod"
 import { AppError } from "../utils/AppError"
 
 export class TablesSessions{
+
+    async index(req: Request, res: Response, next: NextFunction){
+        try {
+            const tablesSessions = await knex<TablesSessionsTypes>("tables-sessions").select()
+
+            res.status(200).json(tablesSessions)
+        } catch (error) {
+            next(error)
+        }
+    }
     async create(req: Request, res: Response, next: NextFunction){
         try {
             const { tables_id } = req.body
@@ -28,7 +38,26 @@ export class TablesSessions{
             next(error)
         }
 
-
-        
     }
+
+    async update(req: Request, res: Response, next: NextFunction){
+        try {
+            const id = z.string().transform((value) => Number(value)).refine((value) => !isNaN(value)).parse(req.params.id)
+            const sessions = await knex<TablesSessionsTypes>("tables-sessions").where({ id }).first()
+
+            if(!sessions){
+            throw new AppError("Essa sessão não existe")
+            }
+
+            if(sessions.closed_at_time){
+                throw new AppError("Sessão já foi fechada")
+            }
+            
+            await knex<TablesSessionsTypes>("tables-sessions").update({ closed_at_time: knex.fn.now()}).where({ id })
+            res.status(200).json()
+        } catch (error) {
+            next(error)
+        }
+    }
+
 }
--- src/routes/tables-sessions.ts
+++ src/routes/tables-sessions.ts
@@ -5,7 +5,9 @@ import { TablesSessions } from "../controller/tables-sessions-controller"
 const tablesSessionsRoutes = Router()
 const tablesSessions = new TablesSessions()
 
+tablesSessionsRoutes.get("/tables-sessions", tablesSessions.index)
 tablesSessionsRoutes.post("/tables-sessions", tablesSessions.create)
+tablesSessionsRoutes.put("/tables-sessions/:id", tablesSessions.update)
 
 export { tablesSessionsRoutes }
 
