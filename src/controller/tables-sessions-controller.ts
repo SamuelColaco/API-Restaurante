d178d6d1f6c45906384a1efc0a016628e71d339d
Samuel <colaco.samuel8@gmail.com>
2024-10-12 13:20:29 -0300

    Verificando a existência de tables-sessions

--- src/controller/tables-sessions-controller.ts
+++ src/controller/tables-sessions-controller.ts
@@ -2,6 +2,7 @@
 import { Request, Response, NextFunction} from "express"
 import { knex } from "../database/knex"
 import z from "zod"
+import { AppError } from "../utils/AppError"
 
 export class TablesSessions{
     async create(req: Request, res: Response, next: NextFunction){
@@ -12,13 +13,22 @@ export class TablesSessions{
                 tables_id: z.number().gt(0)
             })
             bodySchema.parse(req.body)
+            const tableSessionsExist = await knex<TablesSessionsTypes>("tables-sessions").where({ tables_id }).first()
             
             await knex.raw('PRAGMA foreign_keys = ON');
-            await knex<TablesSessionsTypes>("tables-sessions").insert({tables_id,opened_at: knex.fn.now() }) 
+            if(tableSessionsExist === undefined){
+                await knex<TablesSessionsTypes>("tables-sessions").insert({tables_id,opened_at: knex.fn.now() }) 
+            }
+            else{
+                throw new AppError("Essa sessão já existe")
+            }
             
             res.status(201).json()
         } catch (error) {
             next(error)
         }
+
+
+        
     }
 }
